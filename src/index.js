cc11fefeb829a6ba66a36aba89df047e34f48234
Łukasz Rożej <lukasz.rozej@gmail.com>
2019-08-03 11:11:28 +0200

    Draw apple

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const draw = state => {
+  const sizeX = Math.round(canvas.width / state.cols)
+  const sizeY = Math.round(canvas.height / state.rows)
+
+  const x = point => point.x * sizeX
+  const y = point => point.y * sizeY
+
+  // Clear
   ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // Apple
+  ctx.fillStyle = 'red'
+  ctx.fillRect(x(state.apple), y(state.apple), sizeX, sizeY)
 }
 
 draw(initialState)
