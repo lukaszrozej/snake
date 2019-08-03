ea8ba92e79a78bff42fdb11e05614b473fb3e8ce
Łukasz Rożej <lukasz.rozej@gmail.com>
2019-08-03 11:03:18 +0200

    Add canvas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { RIGHT, LEFT, UP, DOWN, initialState } from './game-logic.js'
 
-console.log('Hello!')
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+const draw = state => {
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+draw(initialState)
